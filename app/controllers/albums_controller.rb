e5523c307b9c087f9451613c9d2e1f655c19e009
Rohini <pvrohini86@gmail.com>
2012-07-21 13:38:19 +0530

    changes for album

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -3,7 +3,9 @@ def index
 @albums = Album.where(:user_id => current_user.id)
 end
 def new
+@count = Album.count
 @album = Album.new
+@album = Album.create
 end
 def create
 @album = Album.create(params[:album])
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,9 +1,12 @@
 class PhotosController < ApplicationController
 def new
 @photo=Photo.new
+#@album = Album.find(params[:album_id])
 end
 def create
 @photo=Photo.create(params[:photo])
+@photo.album_id = params[:album_id]
+@photo.save
 end
 def index
 
